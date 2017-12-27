33c34e9ff5017bc9ff3a1d168af25d0206d6511e
alexgurov <avgur89@gmail.com>
2017-12-27 10:49:28 +0200

    02-07. Render list of components

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const GameCard = ({ game }) => (
-    <div className="ui card">
-        <div className="image">
-            <span className="ui green ribbon label">${game.price}</span>
-            <img src={game.thumbnail} alt="Game Cover"/>
+    <div className='ui card'>
+        <div className='image'>
+            <span className='ui green ribbon label'>${game.price}</span>
+            <img src={game.thumbnail} alt='Game Cover' />
         </div>
-        <div className="content">
-            <a href="#" className="header">{game.name}</a>
-            <div className="meta">
-                <i className="icon users" /> {game.players}&nbsp;
-                <i className="icon wait" /> {game.duration} min.
+        <div className='content'>
+            <a href='#' className='header'>{game.name}</a>
+            <div className='meta'>
+                <i className='icon users' /> {game.players}&nbsp;
+                <i className='icon wait' /> {game.duration} min.
             </div>
         </div>
     </div>
