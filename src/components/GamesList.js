233dd3db544eb551f3cf391a49cd67ade84aa67b
alexgurov <avgur89@gmail.com>
2017-12-27 14:47:21 +0200

    02-10. Conditionals and default props

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -4,12 +4,26 @@ import GameCard from './GameCard';
 
 const GamesList = ({ games }) => (
     <div className='ui four cards'>
-        {games.map(game => <GameCard key={game._id} game={game} />)}
+        {games.length === 0 ? (
+            <div className='ui icon message'>
+                <i className='icon info'></i>
+                <div className='content'>
+                    <div className='header'>There are no game in your store!</div>
+                    <p>You should add some!</p>
+                </div>
+            </div>
+        ) : (
+            games.map(game => <GameCard key={game._id} game={game} />)
+        )}
     </div>
 );
 
 GamesList.propTypes = {
     games: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+GamesList.defaultProps = {
+    games: []
 };
 
 export default GamesList;
---
+++ src/components/Price.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Price = ({ cents }) => (
+    <span className='ui green ribbon label'>${(cents / 100).toFixed(2)}</span>
+);
+
+Price.propTypes = {
+    cents: PropTypes.number.isRequired
+};
+
+export default Price;
