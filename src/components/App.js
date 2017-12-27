1da3060cd88aea7fd19c4dec7f5214310701b5c7
alexgurov <avgur89@gmail.com>
2017-12-27 13:23:08 +0200

    PropTypes

--- src/components/App.js
+++ src/components/App.js
@@ -6,25 +6,25 @@ const games = [
         _id: 1,
         name: 'Quadropolis',
         thumbnail: 'https://cf.geekdo-images.com/tthn7L9-fC_GaXJHfA20VdTrFts=/fit-in/1200x630/pic2840020.jpg',
-        price: '32.99',
+        price: 3299,
         players: '2-4',
-        duration: '60'
+        duration: 60
     },
     {
         _id: 2,
         name: 'Five Tribes',
         thumbnail: 'https://cf.geekdo-images.com/o3D15fBxzTt3k2IFZ2u2Xr7Wlyk=/fit-in/246x300/pic2055255.jpg',
-        price: '51.00',
+        price: 5100,
         players: '2-4',
-        duration: '80'
+        duration: 80
     },
     {
         _id: 3,
         name: 'Roll for the Galaxy',
         thumbnail: 'https://cf.geekdo-images.com/Vi3pvbq9sLk_OHzxio8lzjB_77k=/fit-in/246x300/pic1473629.jpg',
-        price: '29.99',
+        price: 2999,
         players: '2-5',
-        duration: '45'
+        duration: 45
     }
 ];
 
