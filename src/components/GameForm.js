5f643dcf54a97633ec0a077872a9fbb29976d5c4
alexgurov <avgur89@gmail.com>
2018-01-02 13:39:30 +0200

    04-03. Textarea field and universal text input handler

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -4,15 +4,16 @@ class GameForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: ''
+            name: '',
+            description: ''
         };
 
-        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleNameChange(e) {
-        this.setState({ name: e.target.value });
+    handleChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
     }
 
     handleSubmit(e) {
@@ -28,9 +29,21 @@ class GameForm extends Component {
                     <input
                         type='text'
                         id='name'
+                        name='name'
                         placeholder='Full game title'
                         value={this.state.name}
-                        onChange={this.handleNameChange}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className='field'>
+                    <label htmlFor='description'>Game Description</label>
+                    <textarea
+                        type='text'
+                        id='description'
+                        name='description'
+                        placeholder='Game Description'
+                        value={this.state.description}
+                        onChange={this.handleChange}
                     />
                 </div>
 
