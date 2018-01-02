756cdb7794e19d078e99500a8dab83379b2b6fa5
alexgurov <avgur89@gmail.com>
2018-01-02 15:13:27 +0200

    04-06. Radio buttons

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,33 +1,47 @@
 import React, { Component } from 'react';
 
+const tags = [
+    { _id: 1, name: 'dice' },
+    { _id: 2, name: 'economic' },
+    { _id: 3, name: 'family' }
+];
+
+const genres = [
+    { _id: 1, name: 'abstract' },
+    { _id: 2, name: 'euro' },
+    { _id: 3, name: 'ameritrash' }
+];
+
 class GameForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            description: '',
-            price: 0,
-            duration: 0,
-            players: ''
-        };
+    state = {
+        name: '',
+        description: '',
+        price: 0,
+        duration: 0,
+        players: '',
+        featured: false,
+        tags: [],
+        genre: 1
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(e) {
-        this.setState({
-            [e.target.name]:
-                e.target.type === 'number'
-                    ? parseInt(e.target.value, 10)
-                    : e.target.value
-        });
-    }
-
-    handleSubmit(e) {
+    handleSubmit = e => {
         e.preventDefault();
         console.log(this.state);
-    }
+    };
+
+    handleStringChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    handleNumberChange = e => this.setState({ [e.target.name]: parseInt(e.target.value, 10) });
+
+    handleCheckboxChange = e => this.setState({ [e.target.name]: e.target.checked });
+
+    toggleTag = tag => {
+        this.state.tags.includes(tag._id)
+            ? this.setState({ tags: this.state.tags.filter(id => id !== tag._id) })
+            : this.setState({ tags: [...this.state.tags, tag._id] });
+    };
+
+    handleGenreChange = genre => this.setState({ genre: genre._id });
 
     render() {
         return (
@@ -40,7 +54,7 @@ class GameForm extends Component {
                         name='name'
                         placeholder='Full game title'
                         value={this.state.name}
-                        onChange={this.handleChange}
+                        onChange={this.handleStringChange}
                     />
                 </div>
                 <div className='field'>
@@ -51,7 +65,7 @@ class GameForm extends Component {
                         name='description'
                         placeholder='Game Description'
                         value={this.state.description}
-                        onChange={this.handleChange}
+                        onChange={this.handleStringChange}
                     />
                 </div>
 
@@ -61,9 +75,9 @@ class GameForm extends Component {
                         <input
                             type='number'
                             id='price'
-                            name='name'
+                            name='price'
                             value={this.state.price}
-                            onChange={this.handleChange}
+                            onChange={this.handleNumberChange}
                         />
                     </div>
                     <div className='field'>
@@ -73,7 +87,7 @@ class GameForm extends Component {
                             id='duration'
                             name='duration'
                             value={this.state.duration}
-                            onChange={this.handleChange}
+                            onChange={this.handleNumberChange}
                         />
                     </div>
                     <div className='field'>
@@ -83,9 +97,54 @@ class GameForm extends Component {
                             id='players'
                             name='players'
                             value={this.state.players}
-                            onChange={this.handleChange}
+                            onChange={this.handleStringChange}
                         />
                     </div>
+                </div>
+
+                <div className='inline field'>
+                    <input
+                        id='featured'
+                        name='featured'
+                        type='checkbox'
+                        checked={this.state.featured}
+                        onChange={this.handleCheckboxChange}
+                    />
+                    <label htmlFor="featured">Featured?</label>
+                </div>
+
+                <div className='field'>
+                    <label>Tags</label>
+                    {
+                        tags.map(tag => (
+                            <div key={tag._id} className='inline field'>
+                                <input
+                                    id={`tag-${tag._id}`}
+                                    type='checkbox'
+                                    checked={this.state.tags.includes(tag._id)}
+                                    onChange={() => this.toggleTag(tag)}
+                                />
+                                <label htmlFor={`tag-${tag._id}`}>{tag.name}</label>
+                            </div>
+                        ))
+                    }
+                </div>
+
+                <div className="field">
+                    <label>Genres</label>
+                    {
+                        genres.map(genre => (
+                            <div key={genre._id} className="inline field">
+                                <input
+                                    id={`genre-${genre._id}`}
+                                    type='radio'
+                                    checked={this.state.genre === genre._id}
+                                    onChange={() => this.handleGenreChange(genre)}
+                                />
+                                <label htmlFor={`genre-${genre._id}`}>{genre.name}</label>
+                            </div>
+                        ))
+                    }
                 </div>
 
                 <button className='ui button' type='submit'>Create</button>
